36dff0f456d1aae60073b75d0c4e2b33ee5de73a
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2021-01-08 13:25:54 +0000

    sandbox: change zoom for national view
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- sandbox/index.js
+++ sandbox/index.js
@@ -32,7 +32,7 @@ function init() {
     map = new OpenLayers.Map({
         div: "map", projection: "EPSG:3857",
         layers: [new OpenLayers.Layer.OSM(), overlay],
-        center: myLocation.getBounds().getCenterLonLat(), zoom: 10
+        center: myLocation.getBounds().getCenterLonLat(), zoom: 6
     });
     // and add the popup to it.
     map.addPopup(popup);
