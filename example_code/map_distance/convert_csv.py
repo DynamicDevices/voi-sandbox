0eefa4d9e2f389899217b3cd8be2b477555c238c
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2021-01-08 15:01:32 +0000

    convert_csv: first cut of parser to convert csv to json and add gps
    Coordinates from Postcode
    
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

---
+++ example_code/map_distance/convert_csv.py
@@ -0,0 +1,27 @@
+import csv
+import json
+from Postcode import Postcode, Distance
+
+csvFilePath = 'input.csv'
+jsonFilePath = 'output.json'
+
+jsonData = []
+
+print("Converting", end="")
+
+with open(csvFilePath) as csvFile:
+  csvReader = csv.DictReader(csvFile)
+  for row in csvReader:
+    print(".", end="", flush=True)
+    if(len(row["POSTCODE"]) > 0):
+      try:
+        p = Postcode(row["POSTCODE"])
+        row["LAT"] = p.lat
+        row["LON"] = p.lon
+      except:
+        pass
+  print("\n")
+
+with open(jsonFilePath, 'w', encoding='ascii') as jsonf:
+  jsonString = json.dumps(jsonData, indent=4)
+  jsonf.write(jsonString)
